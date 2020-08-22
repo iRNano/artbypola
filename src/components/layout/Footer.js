4073ed40ff1478908524bd1be27cf6f2fb2424e3
unknown <valdepenas.adrian@gmail.com>
2020-08-22 19:33:20 +0800

    added styles for footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import pola from "../../assets/images/pola_logo.png";
 
 const Footer = () => {
   return (
@@ -8,7 +9,11 @@ const Footer = () => {
     >
       <div className="row">
         <div className="six wide column">
-          <h3>About POLA</h3>
+          <div className="inline" style={{ display: "flex" }}>
+            <img src={pola} style={{ maxWidth: "70px", padding: "1em" }}></img>
+            <h4 style={{ padding: "1em" }}>About POLA</h4>
+          </div>
+
           <p>
             Aliquip ex sit consequat ullamco commodo duis cillum ipsum do
             laboris nisi tempor cillum. Dolor minim aliquip adipisicing do elit
@@ -17,21 +22,33 @@ const Footer = () => {
             laborum. Sunt
           </p>
         </div>
-        <div className="three wide column">
+        <div className="three wide column" style={{ paddingTop: "3em" }}>
           <h5>Follow Us</h5>
           <div className="social-media-icons">
-            <i className="instagram icon"></i>
-            <i className="youtube icon"></i>
-            <i className="facebook f icon"></i>
+            <i className="instagram icon" style={{ paddingRight: "2em" }}></i>
+            <i className="youtube icon" style={{ paddingRight: "2em" }}></i>
+            <i className="facebook f icon" style={{ paddingRight: "2em" }}></i>
           </div>
         </div>
-        <div className="four wide column">
+        <div
+          className="four wide column"
+          style={{ paddingTop: "3em", paddingRight: "4em" }}
+        >
           <h5>Subscribe</h5>
           <p>Sign up for the latest news, offers and styles</p>
-          <input className="ui input" placeholder="Your email address"></input>
-          <button className="ui button">Subscribe</button>
+          <input
+            className="ui input"
+            placeholder="Your email address"
+            style={{
+              padding: "1em",
+              marginTop: "1em",
+              marginBottom: "1em",
+              width: "100%",
+            }}
+          ></input>
+          <button className="ui secondary button">Subscribe</button>
         </div>
-        <div className="three wide column">
+        <div className="three wide column" style={{ paddingTop: "3em" }}>
           <h5>Contact</h5>
           <p>store@artbypola.com</p>
           <p>8123-12-12</p>
@@ -39,7 +56,10 @@ const Footer = () => {
       </div>
       <div className="row">
         <div className="column">
-          <h5 className="ui header centered" style={{ paddingTop: "4em" }}>
+          <h5
+            className="ui header centered"
+            style={{ paddingTop: "4em", paddingBottom: "2em" }}
+          >
             DESIGN BY PIAO - 2020. ALL RIGHTS RESERVED.
           </h5>
         </div>
